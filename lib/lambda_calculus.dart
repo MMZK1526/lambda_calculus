3db641bb3f8ad01c13c3a3f13d77dd58284f102c
mmzk1526 <yc4120@ic.ac.uk>
2023-07-23 11:39:55 +0100

    [mmzk] (refactor) Export lambda_interface.dart

--- lib/lambda_calculus.dart
+++ lib/lambda_calculus.dart
@@ -2,6 +2,7 @@ export 'package:lambda_calculus/src/lambda_builder.dart';
 export 'package:lambda_calculus/src/lambda_conversion.dart';
 export 'package:lambda_calculus/src/lambda_form.dart';
 export 'package:lambda_calculus/src/lambda_evaluator.dart';
+export 'package:lambda_calculus/src/lambda_interface.dart';
 export 'package:lambda_calculus/src/lambda_parser.dart';
 export 'package:lambda_calculus/src/lambda_type.dart';
 export 'package:lambda_calculus/src/lambda.dart';
